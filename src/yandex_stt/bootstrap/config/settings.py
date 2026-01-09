620de2b659a0191410ba3f2eeef49d308c9b878f
Andrew <49165533+OTR@users.noreply.github.com>
2026-01-08 18:05:53 -0900

    fix: missing variable

--- src/yandex_stt/bootstrap/config/settings.py
+++ src/yandex_stt/bootstrap/config/settings.py
@@ -10,5 +10,5 @@ class Settings(BaseSettings):
         env_file='.env',
         env_file_encoding='utf-8',
         case_sensitive=True,
-        extra='ignore'
+        extra='ignore',
     )
--- src/yandex_stt/main.py
+++ src/yandex_stt/main.py
@@ -3,13 +3,13 @@ import streamlit as st
 from yandex_stt.adapter.yandex_speech_client import YandexSpeechClient
 from yandex_stt.application.recognize_stt_interactor import RecognizeSTTInteractor
 from yandex_stt.bootstrap.config.settings import Settings
-from yandex_stt.presentation.streamlit.st_index_page import display_dummy_text, display_input_audio_form
+from yandex_stt.presentation.streamlit.st_index_page import display_input_audio_form
 
 
 def main() -> None:
     settings: Settings = Settings()
 
-    speech_client: YandexSpeechClient = YandexSpeechClient(api_key=settings.yandex_ai_studio_api_key)
+    speech_client: YandexSpeechClient = YandexSpeechClient(api_key=settings.YANDEX_AI_STUDIO_API_KEY)
     interactor: RecognizeSTTInteractor = RecognizeSTTInteractor(speech_client=speech_client)
 
     display_input_audio_form(interactor=interactor)
